3f27224aa740c2e5d6d3efc024bbf76d07c2d4ba
Portoli <96655364+Portoli@users.noreply.github.com>
2022-11-25 14:47:39 +0100

    "CandleClaw" site addition
    Adding new div .workBoxes for new site "CandleClaw"

--- Contact.js
+++ Contact.js
@@ -1,6 +1,4 @@
-import * as THREE from "../build/three.module.js";
-import { OrbitControls } from "./jsm/controls/OrbitControls.js";
-import Object_ButtonLineMesh from "./Module-DecoButton.js";
+
 let flowFieldAnimation;
 $(document).ready(function () {
   setTimeout(function () {
--- Work.js
+++ Work.js
@@ -1,6 +1,4 @@
-import * as THREE from "../build/three.module.js";
-import { OrbitControls } from "./jsm/controls/OrbitControls.js";
-import Object_ButtonLineMesh from "./Module-DecoButton.js";
+
 $(document).ready(function () {
   setTimeout(function () {
     $("#loadScreen").css("opacity", "0");
